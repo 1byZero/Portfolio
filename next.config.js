f7251fbef58fed845a5099777e5d94a662350c8f
Amit Kumar <amitkp66@live.com>
2023-05-04 22:53:38 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
 }
 
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = {
   async rewrites() {
     return [
       {
